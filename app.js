7dd850e659512ff07b0d456307cf40c025e26783
hansdragnes <hdragnes@gmail.com>
2024-07-11 19:28:47 +0200

    Updated text 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Ci-Cd World!');
+  res.end('Hello, Ci//Cd World!');
 });
 
 const port = process.env.PORT || 3000;
