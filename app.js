1911d3a904b8bb4c1e56b62fbff92ed07c991c2e
hansdragnes <hdragnes@gmail.com>
2024-07-12 15:21:51 +0200

    removed several res.ends

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, Hans, new-featurebranch2!');
-  res.end('New line');
-  res.end('Another new line');
-  res.end('Another new line on main');
+  res.end('Hello, Hans, new-featurebranch2! New line Another new line');
 });
 
 const port = process.env.PORT || 3000;
