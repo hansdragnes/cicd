14f4093243d9ad7cc43505138a9e224bdac0dabb
hansdragnes <hdragnes@gmail.com>
2024-07-12 22:29:59 +0200

    back to start

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Hans, new-featurebranch2! New line Another new line');
+  res.end('Back to sq 1');
 });
 
 const port = process.env.PORT || 3000;
