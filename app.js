673e0ba3a3c44cc019e28809615092d1ebcc79d5
hansdragnes <hdragnes@gmail.com>
2024-07-13 22:36:44 +0200

    nå da

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('sq 2 on hans branch');
+  res.end('ålreit, nå blir det nye deploys on main fra hans branch');
 });
 
 const port = process.env.PORT || 3000;
