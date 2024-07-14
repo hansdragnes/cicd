d939be601374da6d96299196baf79c959e6fe7dd
hansdragnes <hdragnes@gmail.com>
2024-07-14 12:50:42 +0200

    2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('lokal commits på hans branch');
+  res.end('lokal senere commit på hans branch');
 });
 
 const port = process.env.PORT || 3000;
