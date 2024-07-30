97f3154f4b2c1d8decb54f57b1dbbfd2e92ffce0
hansdragnes <hdragnes@gmail.com>
2024-07-30 13:25:53 +0200

    added cosmos db storage cap

--- calc.py
+++ calc.py
@@ -1,2 +1,5 @@
 def square(x):
     return x * x
+
+def add(x,y):
+    return x + y
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 import calc as c
 
@@ -8,9 +8,6 @@ app = Flask(__name__)
 def hello():
     return 'Flask pythonserver', 200, {'Content-Type': 'text/plain'}
 
-@app.route('/health')
-def health():
-    return 'OK', 200, {'Content-Type': 'text/plain'}
 
 @app.route('/calc/<int:number>')
 def calc(number):
@@ -20,6 +17,17 @@ def calc(number):
 @app.route('/links')
 def links():
     return render_template('index.html')
+
+@app.route('/calculator')
+def calculator():
+    return render_template('calc.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    number1 = int(request.form['number1'])
+    number2 = int(request.form['number2'])
+    result = c.add(number1, number2)  # Assuming you have an add function in calc module
+    return f'The result is {result}', 200, {'Content-Type': 'text/plain'}
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
